15093dd61bdbf522cb911c7cf6fcfd00e5d1dc83
Henry Redder <61098446+hredder@users.noreply.github.com>
2023-02-18 14:56:20 -0500

    mouse fix

--- mouse_movement.py
+++ mouse_movement.py
@@ -1,5 +1,8 @@
 import pyautogui
 
 def updateMouse(x,y):
+
+
+    print(x)
     #print(str(x) + " " + str(y))
-    pyautogui.moveTo(1080 - x, y, duration = 0)
+    pyautogui.moveTo(1920-2*x, y, duration = 0)
