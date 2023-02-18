b827ab964a91be2d7f5a0d8a10e44dafef554f47
Henry Redder <61098446+hredder@users.noreply.github.com>
2023-02-18 17:53:44 -0500

    did knn stuff

---
+++ knn.py
@@ -0,0 +1,18 @@
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy
+
+class knn:
+
+    def __init__(self):
+        df = pd.read_csv('completeData.csv', sep=',')
+        self.model = KNeighborsClassifier(n_neighbors=1)
+
+        classifiers = numpy.array(df[df.columns[2]])
+        vectors = numpy.array(df[df.columns[3:100]])
+
+        self.model.fit(classifiers.reshape(-1,1), vectors)
+        self.model.predict()
+
+    def predict(self, vector):
+        return self.model.predict(vector)
