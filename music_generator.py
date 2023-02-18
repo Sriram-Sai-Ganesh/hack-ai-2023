888f45242f08e1543642511f1796b1dc895f2671
Henry Redder <61098446+hredder@users.noreply.github.com>
2023-02-18 10:50:55 -0500

    more dependencies

--- music_generator.py
+++ music_generator.py
@@ -8,3 +8,7 @@ import pandas as pd
 import pretty_midi
 import seaborn as sns
 import tensorflow as tf
+
+from IPython import display
+from matplotlib import pyplot as plt
+from typing import Dict, List, Optional, Sequence, Tuple
