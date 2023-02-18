0f6d8c653975da6b90a927c66457587faf29efcd
Henry Redder <61098446+hredder@users.noreply.github.com>
2023-02-18 11:38:06 -0500

    testing stuff

--- music_generator.py
+++ music_generator.py
@@ -12,3 +12,100 @@ import tensorflow as tf
 from IPython import display
 from matplotlib import pyplot as plt
 from typing import Dict, List, Optional, Sequence, Tuple
+
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+# Sampling rate for audio playback
+_SAMPLING_RATE = 16000
+
+data_dir = pathlib.Path('data/maestro-v2.0.0')
+if not data_dir.exists():
+  tf.keras.utils.get_file(
+      'maestro-v2.0.0-midi.zip',
+      origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
+      extract=True,
+      cache_dir='.', cache_subdir='data',
+  )
+
+filenames = glob.glob(str(data_dir/'**/*.mid*'))
+print('Number of files:', len(filenames))
+
+sample_file = filenames[1]
+print(sample_file)
+
+pm = pretty_midi.PrettyMIDI('test.mid')
+
+instrument = pm.instruments[0]
+
+for i, note in enumerate(instrument.notes[:10]):
+  note_name = pretty_midi.note_number_to_name(note.pitch)
+  duration = note.end - note.start
+  print(f'{i}: pitch={note.pitch}, note_name={note_name},'
+        f' duration={duration:.4f}')
+
+
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+  pm = pretty_midi.PrettyMIDI(midi_file)
+  instrument = pm.instruments[0]
+  notes = collections.defaultdict(list)
+
+  # Sort the notes by start time
+  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+  prev_start = sorted_notes[0].start
+
+  for note in sorted_notes:
+    start = note.start
+    end = note.end
+    notes['pitch'].append(note.pitch)
+    notes['start'].append(start)
+    notes['end'].append(end)
+    notes['step'].append(start - prev_start)
+    notes['duration'].append(end - start)
+    prev_start = start
+
+  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+raw_notes = midi_to_notes(sample_file)
+print(raw_notes.head())
+
+get_note_names = np.vectorize(pretty_midi.note_number_to_name)
+sample_note_names = get_note_names(raw_notes['pitch'])
+sample_note_names[:10]
+  
+#FUNCTION
+def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
+  if count:
+    title = f'First {count} notes'
+  else:
+    title = f'Whole track'
+    count = len(notes['pitch'])
+  plt.figure(figsize=(20, 4))
+  plot_pitch = np.stack([notes['pitch'], notes['pitch']], axis=0)
+  plot_start_stop = np.stack([notes['start'], notes['end']], axis=0)
+  plt.plot(
+      plot_start_stop[:, :count], plot_pitch[:, :count], color="b", marker=".")
+  plt.xlabel('Time [s]')
+  plt.ylabel('Pitch')
+  _ = plt.title(title)
+
+plot_piano_roll(raw_notes)
+plt.show()
+
+#FUNCTION
+def plot_distributions(notes: pd.DataFrame, drop_percentile=2.5):
+  plt.figure(figsize=[15, 5])
+  plt.subplot(1, 3, 1)
+  sns.histplot(notes, x="pitch", bins=20)
+
+  plt.subplot(1, 3, 2)
+  max_step = np.percentile(notes['step'], 100 - drop_percentile)
+  sns.histplot(notes, x="step", bins=np.linspace(0, max_step, 21))
+
+  plt.subplot(1, 3, 3)
+  max_duration = np.percentile(notes['duration'], 100 - drop_percentile)
+  sns.histplot(notes, x="duration", bins=np.linspace(0, max_duration, 21))
+
+plot_distributions(raw_notes)
+plt.show()
