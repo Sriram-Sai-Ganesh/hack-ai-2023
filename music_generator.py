cf62fddfb80c07b364652effda07b59d1fa39689
Henry Redder <61098446+hredder@users.noreply.github.com>
2023-02-18 10:48:50 -0500

    initial music generator python file created

---
+++ music_generator.py
@@ -0,0 +1,10 @@
+import collections
+import datetime
+import fluidsynth
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+import seaborn as sns
+import tensorflow as tf
